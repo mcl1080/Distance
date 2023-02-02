29967a2376afc289f8c19782c083483a47e74d43
Mike Laren <mcl768@gmail.com>
2023-02-01 21:30:16 -0800

    Added a function to allow DMS, DDM, or DD inputs using N,E,S,W directions or just negative signs.

--- Distance.py
+++ Distance.py
@@ -11,7 +11,7 @@ def distance_between_points(lat1, lon1, lat2, lon2):
     lon2 = radians(lon2)
 
     # The radius of Earth in miles
-    r = 3958.755866
+    r = 3958.755866 * 1609.34
 
     # Haversine formula to calculate distance
     d_lon = lon2 - lon1
@@ -39,8 +39,8 @@ def distance_between_polar_points(r1, theta1, r2, theta2):
     angle. Then uses those distances and angles as polar coordinates which are converted to cartesian coordinates and
     the distance is calculated using the pythagorean theorem."""
     # Convert latitude
-    r1 = convert_latitude(r1) * 69.09332414433953
-    r2 = convert_latitude(r2) * 69.09332414433953
+    r1 = convert_latitude(r1) * 69.09332414433953 * 1609.34
+    r2 = convert_latitude(r2) * 69.09332414433953 * 1609.34
 
     # Convert polar coordinates to cartesian coordinates
     x1 = r1 * cos(radians(theta1))
@@ -54,15 +54,55 @@ def distance_between_polar_points(r1, theta1, r2, theta2):
     return distance
 
 
-print("Input Decimal Coordinates in this format 37.233, -115.812. Decimal Degrees separated by comma space.")
+def parse_coordinate(coordinate):
+    # split the input string into direction and values
+    parts = coordinate.upper().split()
+    if parts[0] in ["N", "S", "W", "E"]:
+        direction = parts[0]
+        values = parts[1:]
+    else:
+        direction = "none"
+        values = parts
+    # determine the format of the values
+    if len(values) == 3:
+        # degrees-minutes-seconds format
+        minutes = int(values[-2])
+        seconds = float(values[-1])
+        decimal_minutes = minutes / 60 + seconds / 3600
+        decimal_degrees = abs(int(values[0])) + decimal_minutes
+    elif len(values) == 2:
+        # degrees-decimal minutes format
+        minutes = float(values[-1])
+        decimal_minutes = minutes / 60
+        decimal_degrees = abs(int(values[0])) + decimal_minutes
+    else:
+        # decimal degrees format
+        decimal_degrees = float(values[0])
+
+    # determine the sign based on the direction or negative values
+    sign = 1
+    if direction in ["S", "W"] or values[0][0] == "-":
+        sign = -1
+
+    # return the decimal degrees with the correct sign
+    return sign * decimal_degrees
+
+
+print("Input Coordinates in any of these formats DMS, DDM, or DD. You can use N,E,S,W or negative signs.")
+print("Separate each part with a space, and separate Lat and Lon with comma space.")
 while True:
-    lat_1, lon_1 = list(map(float, input("Coordinates 1: ").split(", ")))
-    lat_2, lon_2 = list(map(float, input("Coordinates 2: ").split(", ")))
+    lat_1, lon_1 = list(input("Coordinates 1: ").split(", "))
+    lat_2, lon_2 = list(input("Coordinates 2: ").split(", "))
+
+    lat_1 = parse_coordinate(lat_1)
+    lon_1 = parse_coordinate(lon_1)
+    lat_2 = parse_coordinate(lat_2)
+    lon_2 = parse_coordinate(lon_2)
 
     g_dist = distance_between_points(lat_1, lon_1, lat_2, lon_2)
-    print(f"Globe: {round(g_dist, 3)} Miles")
+    print(f"Globe: {round(g_dist, 3)} Meters")
     f_dist = distance_between_polar_points(lat_1, lon_1, lat_2, lon_2)
-    print(f"Flat: {round(f_dist, 3)} Miles")
+    print(f"Flat: {round(f_dist, 3)} Meters")
     dif = f_dist - g_dist
-    print(f"Difference: {round(dif, 3)} Miles")
+    print(f"Difference: {round(dif, 3)} Meters")
     print(f"-------------------------------")
