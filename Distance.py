67368fde6c3adcca67e4afed1a46775e61ab7e77
Mike Laren <mcl768@gmail.com>
2023-02-03 11:29:45 -0800

    Adjusted radius, and distance per N-S degree, to more accurate numbers.

--- Distance.py
+++ Distance.py
@@ -11,7 +11,7 @@ def distance_between_points(lat1, lon1, lat2, lon2):
     lon2 = radians(lon2)
 
     # The radius of Earth in miles
-    r = 3958.755866 * 1609.34
+    r = 3956.0883381231684613995179014794
 
     # Haversine formula to calculate distance
     d_lon = lon2 - lon1
@@ -38,9 +38,9 @@ def distance_between_polar_points(r1, theta1, r2, theta2):
     """ This Function takes the lat and lon, converts the lat to a distance from the North Pole, and the lon as the
     angle. Then uses those distances and angles as polar coordinates which are converted to cartesian coordinates and
     the distance is calculated using the pythagorean theorem."""
-    # Convert latitude
-    r1 = convert_latitude(r1) * 69.09332414433953 * 1609.34
-    r2 = convert_latitude(r2) * 69.09332414433953 * 1609.34
+    # Convert latitude to miles
+    r1 = convert_latitude(r1) * 69.046767
+    r2 = convert_latitude(r2) * 69.046767
 
     # Convert polar coordinates to cartesian coordinates
     x1 = r1 * cos(radians(theta1))
@@ -100,9 +100,9 @@ while True:
     lon_2 = parse_coordinate(lon_2)
 
     g_dist = distance_between_points(lat_1, lon_1, lat_2, lon_2)
-    print(f"Globe: {round(g_dist, 3):,} Meters")
+    print(f"Globe: {round(g_dist, 3):,} Miles")
     f_dist = distance_between_polar_points(lat_1, lon_1, lat_2, lon_2)
-    print(f"Flat: {round(f_dist, 3):,} Meters")
+    print(f"Flat: {round(f_dist, 3):,} Miles")
     dif = f_dist - g_dist
-    print(f"Difference: {round(dif, 3):,} Meters")
+    print(f"Difference: {round(dif, 3):,} Miles")
     print(f"-------------------------------")
